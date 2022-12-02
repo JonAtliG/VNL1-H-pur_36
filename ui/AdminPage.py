0933b28cc030950f5da1621e6d28d5e42e422c6a
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-02 22:58:09 +0000

    Update AdminPage.py

--- ui/AdminPage.py
+++ ui/AdminPage.py
@@ -1,4 +1,5 @@
 #from MainMenu import MainMenu?
+from DisplayCTP import DisplayCTP
 #from that other thing import another thing
 #etc
 
@@ -6,8 +7,8 @@ class AdminPage():
 
 
     def __str__(self) -> str:
-        pass
-        #return f"{self.adminpage_output()}{self.input_prompt()}"
+        
+        return f"{self.adminpage_output()}{self.input_prompt()}"
 
     def adminpage_output(self) -> str:
         print("Welcome, Admin")
@@ -23,40 +24,52 @@ class AdminPage():
         """)
 
     def input_prompt(self):
-        choice = input("Select an option: ")
-        #if option 1, 2, 3, 4, run programs for 1, 2, 3, 4
-        #if option 5, 6, run display for 5, 6
-        if choice == '1':
-            pass
-            #host = create_host()
-        elif choice == '2':
-            pass
-            #player = Player()
-            #player.name = input name
-            #player.address = input address
-            #player.email = etc
-            #player.telephone = 
-            #player.mobile = 
-            #player = create_player()
-            #list_of_players.append(player)
-            #return player
-        if choice == '3':
-            pass
-            #list_of_teams.append(team)
-            #return team
-        elif choice == '4':
-            pass
-            #club = Club()
-            #club_name = input("Enter club name: ")
-            #create_club = create_club()
-            #club_list.append(club)
-            #return club?? or create_club
-        elif choice == '5':
-            pass
-            #display current tournament information
-        elif choice == '6':
-            pass # team, clubs, players
-        elif choice == 'q':
-            pass #go back to main menu if possible?
-        else:
-            print("Invalid option")
+        while True:
+
+            club_list = []
+            team_list = []
+            player_list = []
+
+            choice = input("Select an option: ")
+
+            if choice == '1':
+                pass
+                #host = create_host()
+
+            elif choice == '2':
+                pass
+                #player = Player()
+                #player.name =
+                #player.address = 
+                #player.email = 
+                #player.telephone = 
+                #player.mobile = 
+                #player = create_player()
+                #player_list.append(player)
+                #return player
+
+            elif choice == '3':
+                pass
+                #team_list.append(team)
+                #return team
+
+            elif choice == '4':
+                pass
+                #club = Club()
+                #club_name = input("Enter club name: ")
+                #create_club = create_club()
+                #club_list.append(club)
+                #return club?? or create_club
+
+            elif choice == '5':
+                pass
+                #display current tournament information
+
+            elif choice == '6':
+                view = DisplayCTP("club list", "team list", "player list")
+
+            elif choice == 'q':
+                pass #go back to main menu if possible?
+
+            else:
+                print("Invalid option")
