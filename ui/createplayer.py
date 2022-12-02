c21ca520a071713a2eb5c794dd4de33249a7980c
rebekkarosin <112619663+rebekkarosin@users.noreply.github.com>
2022-12-02 13:37:57 +0000

    Add files via upload

---
+++ ui/createplayer.py
@@ -0,0 +1,26 @@
+
+class Createplayer():
+
+    def getid(self):
+        id_input = input("Player ID: ")
+        return id_input
+
+    def getname(self):
+        playername= input("Player name: ")
+        return playername
+
+    def getaddress(self):
+        playeraddress= input("Player address: ")
+        return playeraddress
+    
+    def getphonenumber(self):
+        phonenumber= input("Player phone-number: ")
+        return phonenumber
+
+    def gettelephonenumber(self):
+        telephonenumber= input("Player telephone-number: ")
+        return telephonenumber
+
+    def getemailaddress(self):
+        emailaddress = input("Player emailaddress: ")
+        return emailaddress
