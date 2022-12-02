d1f64149c99b07a1f8770901b19774db27d231cd
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-02 22:46:53 +0000

    Update AdminPage.py

--- ui/AdminPage.py
+++ ui/AdminPage.py
@@ -1,17 +1,13 @@
 #from MainMenu import MainMenu?
-from CreateTeam import CreateTeam
-from CreateClub import CreateClub
 #from that other thing import another thing
 #etc
 
 class AdminPage():
 
-    def __init__(self) -> None:
-        pass #no arguments held eg? User/pass?
 
     def __str__(self) -> str:
-        
-        return f"{self.adminpage_output()}{self.input_prompt()}"
+        pass
+        #return f"{self.adminpage_output()}{self.input_prompt()}"
 
     def adminpage_output(self) -> str:
         print("Welcome, Admin")
@@ -20,7 +16,7 @@ class AdminPage():
         2. Create Player
         3. Create Team
         4. Create Club
-        5. View Current Tournament
+        5. View Tournament
         6. View all Teams, Clubs and Players
         
         'q' to Logout
@@ -30,18 +26,36 @@ class AdminPage():
         choice = input("Select an option: ")
         #if option 1, 2, 3, 4, run programs for 1, 2, 3, 4
         #if option 5, 6, run display for 5, 6
-        #if choice == '1':
-            #create host
-        #elif choice == '2':
-            #create player (playerinfo??)
+        if choice == '1':
+            pass
+            #host = create_host()
+        elif choice == '2':
+            pass
+            #player = Player()
+            #player.name = input name
+            #player.address = input address
+            #player.email = etc
+            #player.telephone = 
+            #player.mobile = 
+            #player = create_player()
+            #list_of_players.append(player)
+            #return player
         if choice == '3':
-            create_team = CreateTeam()
-            print(create_team)
+            pass
+            #list_of_teams.append(team)
+            #return team
         elif choice == '4':
-            create_club = CreateClub()
-            print(create_club)
-        #elif choice == '5':
+            pass
+            #club = Club()
+            #club_name = input("Enter club name: ")
+            #create_club = create_club()
+            #club_list.append(club)
+            #return club?? or create_club
+        elif choice == '5':
+            pass
             #display current tournament information
+        elif choice == '6':
+            pass # team, clubs, players
         elif choice == 'q':
             pass #go back to main menu if possible?
         else:
