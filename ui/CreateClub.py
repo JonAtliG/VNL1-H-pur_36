307f99146f385c892765be8bc344ea4172f5da1a
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-02 10:41:41 +0000

    Add files via upload

---
+++ ui/CreateClub.py
@@ -0,0 +1,14 @@
+
+class CreateClub():
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def get_name(self):
+        pass
+
+    def add_team(self):
+        pass
---
+++ ui/CreateTeam.py
@@ -0,0 +1,15 @@
+from MainMenu import MainMenu
+
+class CreateTeam():
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def get_name(self):
+        pass
+
+    def add_player(self, ID):
+        pass
