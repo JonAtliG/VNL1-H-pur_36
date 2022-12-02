fdb366ddecc6f7f8962f077b9ff666a5715c5bd7
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-02 13:22:21 +0000

    Update CreateTeam.py

--- ui/CreateTeam.py
+++ ui/CreateTeam.py
@@ -2,14 +2,20 @@ from MainMenu import MainMenu
 
 class CreateTeam():
 
-    def __init__(self) -> None:
-        pass
 
     def __str__(self) -> str:
-        pass
+        pass # Geta séð Player samkvæmt kennitölu (ID) sem sett er inn)
+    #Returnaði listinn úr add_player setur upp
 
-    def get_name(self):
-        pass
+    def get_name(self, name, ):
+        name = input("Enter team name: ")
+        return name
 
-    def add_player(self, ID):
-        pass
+
+    def add_player(self, list, ID):
+        self.list = list
+        self.id = ID
+        list = []
+        list.append(ID)
+
+        return list
