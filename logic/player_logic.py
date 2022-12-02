7b70ba9c1ea13a2f930074bc15c954b32fb61102
JonAtliG <111296050+JonAtliG@users.noreply.github.com>
2022-12-02 14:12:13 +0000

    Update player_logic.py

--- logic/player_logic.py
+++ logic/player_logic.py
@@ -4,6 +4,10 @@ class Player_Logic():
     def __init__():
         pass
     
+    def get_all_players(self, players):
+        '''takes in players and converts to dictonary and returns'''
+        pass
+    
     def create_player(self):
         '''Takes in player object and forwards to data layer'''
         pass
