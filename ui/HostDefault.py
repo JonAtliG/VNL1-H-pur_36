a54d8408bdd1f91b5eecd6431b834342151f2e4e
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-05 14:08:17 +0000

    Add files via upload

---
+++ ui/HostDefault.py
@@ -0,0 +1,23 @@
+from VerklegtH22.MainMenu import MainMenu
+#logging in with host ID
+
+class HostDefault():
+
+    def __init__(self) -> None:
+        pass
+
+    def options(self):
+        print("""
+    Please select an option:
+        1. Create tournament
+        2. Change match
+        3. View matches played
+        4. View teams and players
+        'q' to logout.""")
+        print("_"*25)
+
+    def input_prompt():
+        pass
+
+    def __str__(self) -> str:
+        pass
