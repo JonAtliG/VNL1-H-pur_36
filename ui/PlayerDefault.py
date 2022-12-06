90ee749349cbe61d1bcf87f3b7cd9b1da1075c29
julzkur <112621521+julzkur@users.noreply.github.com>
2022-12-06 23:56:54 +0000

    Update PlayerDefault.py

--- ui/PlayerDefault.py
+++ ui/PlayerDefault.py
@@ -18,7 +18,8 @@ class PlayerDefault():
         2. View player matches
         3. View all teams, clubs, players
 
-        'q' to quit""")
+        'q' to logout
+        """)
 
     def input_prompt(self, ID):
         self.id = ID
@@ -29,7 +30,7 @@ class PlayerDefault():
 
             if option == '1':
                 self.information(ID)
-                go_back = input("Enter 'q' to go back: ")
+                go_back = input("\nEnter 'q' to go back: ")
                 if go_back == 'q':
                     continue
             elif option == '2':
@@ -52,11 +53,11 @@ class PlayerDefault():
         """)
         player_input = input("Enter option: ")
         if player_input == '1':
-            self.display_all.display_all_players(self.logic_wrapper.get_all_players())
+            self.DisplayAll.display_all_players(self.logic_wrapper.get_all_players())
         elif player_input == '2':
-            self.display_all.display_all_teams(self.logic_wrapper.get_all_teams())
+            self.DisplayAll.display_all_teams(self.logic_wrapper.get_all_teams())
         elif player_input == '3':
-            self.display_all.display_all_clubs(self.logic_wrapper.get_all_clubs())
+            self.DisplayAll.display_all_clubs(self.logic_wrapper.get_all_clubs())
         elif player_input == 'q':
             return
 
@@ -64,7 +65,7 @@ class PlayerDefault():
     def information(self, ID):
         self.logic_wrapper.get_player_by_id(ID)
         print("\nPlayer information:\n")
-        print("{:>10} {}".format("Team: ", self.player.team))
+        print("{:<15} {}".format("Team: ", self.player.team))
         print("{:<15} {}".format("Name:", self.player.name))
         print("{:<15} {}".format("ID:", self.player.nid))
         print("{:<15} {}".format("Birthdate:", self.player.birthdate))
