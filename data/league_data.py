4b87436dde0229d9a1b88d2f567d0fbae0d505b7
JonAtliG <jonatli99@gmail.com>
2022-12-08 19:19:47 +0000

    add teams to league

--- data/league_data.py
+++ data/league_data.py
@@ -15,12 +15,12 @@ class League_Data():
         if league.matches == "No matches":
             league_data += "No matches;"
         else:
-            league_data += ",".join([match.ID for match in league.matches]) + ";"
+            league_data += ",".join([match.id for match in league.matches]) + ";"
         league_data += f"{league.start_date};{league.end_date}"
         return league_data
 
-    def __get_league_index_by_id(self, id: str):
-        return self.__CSV_Handler.get_line_index_by_data(id, 0)
+    def __get_league_index_by_name(self, name: str):
+        return self.__CSV_Handler.get_line_index_by_data(name, 0)
     
     def get_league_data_by_name(self, name: str) -> list:
         return self.__CSV_Handler.get_data_by_data(name, 0)
@@ -29,7 +29,7 @@ class League_Data():
         return self.__CSV_Handler.get_all_data()
 
     def update_league(self, league: League) -> None:
-        index = self.__get_league_index_by_id(league.id)
+        index = self.__get_league_index_by_name(league.name)
         league_data = self.__create_league_data_from_object(league)
         self.__CSV_Handler.replace_line(index, league_data)
 
--- model/match.py
+++ model/match.py
@@ -1,6 +1,6 @@
 class Match():
-    def __init__(self, ID = None, home_team = None, away_team = None, games = None, date = None) -> None:
-        self.ID = ID
+    def __init__(self, id = None, home_team = None, away_team = None, games = None, date = None) -> None:
+        self.id = id
         self.home_team = home_team
         self.away_team = away_team
         self.games = games
