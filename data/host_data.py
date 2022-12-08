8a9c4c002d485c63fa6df3d318c1c32953f7a4d2
JonAtliG <jonatli99@gmail.com>
2022-12-08 13:41:25 +0000

    auka host data

--- data/host_data.py
+++ data/host_data.py
@@ -15,5 +15,11 @@ class Host_Data():
                 return True
         return False
     
+    def get_league_names_by_id(self, id):
+        return self.__CSV_Handler.get_data_by_data(id, 0)[2].strip("\n").split(",")
+    
+    def get_host_name_by_id(self, id):
+        return self.__CSV_Handler.get_data_by_data(id, 0)
+    
     def add_host(self, id):
         self.__CSV_Handler.add_line(id)
