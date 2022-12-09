3af6f3e7fdcf8b2ca9ba6c937454db8f90e7faaf
JonAtliG <jonatli99@gmail.com>
2022-12-09 13:36:58 +0000

    comments

--- logic/host_logic.py
+++ logic/host_logic.py
@@ -4,7 +4,8 @@ class Host_logic():
     def __init__(self, data_connection) -> None:
         self.__data_wrapper = data_connection
     
-    def __make_host_object_from_data(self, data):
+    def __make_host_object_from_data(self, data: list) -> Host:
+        '''Creates host object from the data and returns it'''
         host = Host()
         host.id = data[0]
         host.name = data[1]
@@ -15,16 +16,21 @@ class Host_logic():
         return host
     
     def verify_id(self, id):
+        '''Forwards id to data_wrapper, returns true if the ID exists in the file'''
         return self.__data_wrapper.verify_host_id(id)
     
-    def get_host_by_league_name(self, name):
+    def get_host_by_league_name(self, name: str) -> Host:
+        '''Returns the host object, for the given name'''
         return self.__make_host_object_from_data(self.__data_wrapper.get_host_data_by_league_name(name))
     
-    def get_host_by_id(self, id):
+    def get_host_by_id(self, id: str) -> Host:
+        '''Returns the host object, for the given ID'''
         return self.__make_host_object_from_data(self.__data_wrapper.get_host_data_by_id(id))
     
-    def add_host(self, host):
+    def add_host(self, host: Host) -> None:
+        '''Forwards host object to data_wrapper, to add to the file'''
         self.__data_wrapper.add_host(host)
     
-    def update_host(self, host):
+    def update_host(self, host: Host) -> None:
+        '''Forwards host object to data_wrapper, to add to the file'''
         self.__data_wrapper.update_host(host)
