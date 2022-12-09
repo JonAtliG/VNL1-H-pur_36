d0208b5dcff6feedf8436238fd59401497fe769e
Einar Andri <einarandri03@gmail.com>
2022-12-09 20:27:57 +0000

    The end is now

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from ui.MainMenu import MainMenu
 
 #Main function for the program.
-#try:
-mainmenu = MainMenu()
-mainmenu.input_prompt()
-#except:
-#    print("An unexpected error occured, please try again.")
+try:
+    MainMenu().input_prompt()
+except:
+    print("An unexpected error occured, please try again.")
